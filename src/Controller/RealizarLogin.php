f45fb70869c9df833c364415ea32f772c450015b
Bruna <bruna0chan@gmail.com>
2021-01-02 01:18:43 -0300

    creating login

---
+++ src/Controller/RealizarLogin.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Usuario;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+class RealizarLogin extends ControllerComHtml implements InterfaceControladorRequisicao
+{
+    private $repositorioUsuarios;
+
+    public function __construct()
+    {
+        $entityManager = (new EntityManagerCreator())
+            ->getEntityManager();
+        $this->repositorioUsuarios = $entityManager
+            ->getRepository(Usuario::class);
+    }
+
+    public function processaRequisicao(): void
+    {
+        $email = filter_input(
+            INPUT_POST,
+            'email',
+            FILTER_VALIDATE_EMAIL
+        );
+        if (is_null($email) || $email === false) {
+            $titulo = "E-mail";
+            require __DIR__ . '/../../view/alerta.php';
+            return;
+        }
+
+        $senha = filter_input(
+            INPUT_POST,
+            'senha',
+            FILTER_SANITIZE_STRING
+        );
+
+        /** @var Usuario usuario **/
+
+        $usuario = $this->repositorioUsuarios
+            ->findOneBy(['email' => $email]);
+
+        if (is_null($usuario) || !$usuario->senhaEstaCorreta($senha)) {
+            $titulo = "E-mail ou senha";
+            require __DIR__ . '/../../view/alerta.php';
+            return;
+        }
+
+        header('Location: /listar-cursos');
+    }
+}
