64bd970dd39f2be5b0f790d4a5c405dad5daca34
Bruna <bruna0chan@gmail.com>
2021-01-03 19:11:03 -0300

    changing FormularioEdicao Controller

--- src/Controller/FormularioEdicao.php
+++ src/Controller/FormularioEdicao.php
@@ -5,38 +5,43 @@ namespace Alura\Cursos\Controller;
 use Alura\Cursos\Entity\Curso;
 use Alura\Cursos\Helper\RenderizadorDeHtmlTrait;
 use Alura\Cursos\Infra\EntityManagerCreator;
+use Doctrine\ORM\EntityManagerInterface;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class FormularioEdicao implements InterfaceControladorRequisicao
+class FormularioEdicao implements
+    RequestHandlerInterface
 {
     use RenderizadorDeHtmlTrait;
 
     private $repositorioCursos;
 
-    public function __construct()
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $entityManager = (new EntityManagerCreator())
-            ->getEntityManager();
         $this->repositorioCursos = $entityManager
             ->getRepository(Curso::class);
     }
 
-    public function processaRequisicao(): void
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $id = filter_input(
-            INPUT_GET,
-            'id',
+        $id = filter_var(
+            $request->getQueryParams()['id'],
             FILTER_VALIDATE_INT
         );
 
+        $response = new Response(302, ['Location' => '/listar-cursos']);
+
         if (is_null($id) || $id === false) {
-            header('Location: /listar-cursos');
-            return;
+            return $response;
         }
 
         $curso = $this->repositorioCursos->find($id);
-        echo $this->renderizaHtml('cursos/formulario.php', [
+        $html = $this->renderizaHtml('cursos/formulario.php', [
             'curso' => $curso,
             'titulo' => 'Alterar Curso'
         ]);
+        return new Response(200, [], $html);
     }
 }
