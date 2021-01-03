841e08e9465cf6802f96728fc6e43ab852cec408
Bruna <bruna0chan@gmail.com>
2021-01-03 19:32:31 -0300

    chaging Persistencia Controller

--- src/Controller/Persistencia.php
+++ src/Controller/Persistencia.php
@@ -5,33 +5,35 @@ namespace Alura\Cursos\Controller;
 use Alura\Cursos\Entity\Curso;
 use Alura\Cursos\Helper\FlashMessageTrait;
 use Alura\Cursos\Infra\EntityManagerCreator;
+use Doctrine\ORM\EntityManagerInterface;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class Persistencia implements InterfaceControladorRequisicao
+class Persistencia implements
+    RequestHandlerInterface
 {
     use FlashMessageTrait;
 
     private $entityManager;
 
-    public function __construct()
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->entityManager = (new EntityManagerCreator())
-            ->getEntityManager();
     }
 
-    public function processaRequisicao(): void
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $descricao = filter_input(
-            INPUT_POST,
-            'descricao',
+        $descricao = filter_var(
+            $request->getParsedBody()['descricao'],
             FILTER_SANITIZE_STRING
         );
+
         $curso = new Curso();
-        $curso->setDescricao($_POST['descricao']);
+        $curso->setDescricao($request->getParsedBody()['descricao']);
 
-
-        $id = filter_input(
-            INPUT_GET,
-            'id',
+        $id = filter_var(
+            $request->getQueryParams()['id'],
             FILTER_VALIDATE_INT
         );
 
@@ -46,6 +48,6 @@ class Persistencia implements InterfaceControladorRequisicao
 
         $this->entityManager->flush();
 
-        header('Location: /listar-cursos', true, 302);
+        return new Response(302, ['Location' => '/listar-cursos']);
     }
 }
